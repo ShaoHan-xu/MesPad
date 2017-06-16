38a3e01a68154aca88f7b0cafe46e31a134b4e04
shawn-xu <private_xsh@163.com>
2017-06-16 15:34:52 +0800

    ~~

--- app/src/main/java/com/eeka/mespad/activity/MainFragment.java
+++ app/src/main/java/com/eeka/mespad/activity/MainFragment.java
@@ -1,6 +1,5 @@
 package com.eeka.mespad.activity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,16 +10,21 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.alibaba.fastjson.JSONObject;
-import com.bumptech.glide.Glide;
 import com.eeka.mespad.R;
 import com.eeka.mespad.adapter.CommonAdapter;
 import com.eeka.mespad.adapter.ViewHolder;
+import com.eeka.mespad.bo.MaterialsBo;
 import com.eeka.mespad.http.HttpHelper;
 
 import java.lang.ref.WeakReference;
@@ -36,12 +40,16 @@ public class MainFragment extends BaseFragment {
 
     private ViewPager mViewPager;
     private ViewPagerAdapter mViewPagerAdapter;
-    private List<Integer> mList_data;
+    private List<Integer> mList_processData;
 
     private RadioGroup mRadioGroup;
 
-    private ListView mLv_process;
+    private ExpandableListView mElv_process;
     private ProcessAdapter mProcessAdapter;
+
+    private LinearLayout mLayout_materials;
+    private GridView mGv_materials;
+    private List<MaterialsBo> mList_materialsData;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -66,25 +74,26 @@ public class MainFragment extends BaseFragment {
         mRadioGroup = (RadioGroup) mView.findViewById(R.id.rg_main_indication);
         mRadioGroup.setOnCheckedChangeListener(new RadioChangedListener());
 
-        mView.findViewById(R.id.iv_materials).setOnClickListener(this);
         mView.findViewById(R.id.btn_returnMaterials).setOnClickListener(this);
         mView.findViewById(R.id.btn_getMaterials).setOnClickListener(this);
 
-        mLv_process = (ListView) mView.findViewById(R.id.lv_processCode);
+        mElv_process = (ExpandableListView) mView.findViewById(R.id.elv_processCode);
+        mLayout_materials = (LinearLayout) mView.findViewById(R.id.layout_materials);
+        mGv_materials = (GridView) mView.findViewById(R.id.gv_materials);
     }
 
     protected void initData() {
-        mList_data = new ArrayList<>();
-        mList_data.add(R.drawable.libai);
-        mList_data.add(R.drawable.hanxin);
-        mList_data.add(R.drawable.zhaoyun);
-        mList_data.add(R.drawable.renzhe);
+        mList_processData = new ArrayList<>();
+        mList_processData.add(R.drawable.libai);
+        mList_processData.add(R.drawable.hanxin);
+        mList_processData.add(R.drawable.zhaoyun);
+        mList_processData.add(R.drawable.renzhe);
 
         mViewPagerAdapter = new ViewPagerAdapter();
         mViewPager.setAdapter(mViewPagerAdapter);
         mViewPager.addOnPageChangeListener(new ViewPagerChangedListener());
 
-        for (int i = 0; i < mList_data.size(); i++) {
+        for (int i = 0; i < mList_processData.size(); i++) {
             RadioButton rb = new RadioButton(mContext);
             rb.setEnabled(false);
             mRadioGroup.addView(rb);
@@ -93,24 +102,39 @@ public class MainFragment extends BaseFragment {
             }
         }
 
-        List<String> list = new ArrayList<>();
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        mProcessAdapter = new ProcessAdapter(mContext, list, R.layout.lv_item_process);
-        mLv_process.setAdapter(mProcessAdapter);
+        List<String> group = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            group.add("");
+        }
+        List<List<String>> child = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            List<String> list = new ArrayList<>();
+            for (int j = 0; j < 5; j++) {
+                list.add("");
+            }
+            child.add(list);
+        }
+        mProcessAdapter = new ProcessAdapter(group, child);
+        mElv_process.setAdapter(mProcessAdapter);
+
+        mList_materialsData = new ArrayList<>();
+        mList_materialsData.add(new MaterialsBo("" + R.drawable.materials1, "面料1"));
+        mList_materialsData.add(new MaterialsBo("" + R.drawable.materials2, "面料2"));
+        mList_materialsData.add(new MaterialsBo("" + R.drawable.materials3, "面料3"));
+        mList_materialsData.add(new MaterialsBo("" + R.drawable.hanxin, "面料4"));
+        mList_materialsData.add(new MaterialsBo("" + R.drawable.libai, "面料5"));
+        mList_materialsData.add(new MaterialsBo("" + R.drawable.zhaoyun, "面料6"));
+        mList_materialsData.add(new MaterialsBo("" + R.drawable.renzhe, "面料7"));
+        mGv_materials.setAdapter(new CommonAdapter<MaterialsBo>(mContext, mList_materialsData, R.layout.item_textview) {
+            @Override
+            public void convert(ViewHolder holder, MaterialsBo item, int position) {
+                holder.setText(R.id.text, item.getName());
+            }
+        });
+
+        for (MaterialsBo item : mList_materialsData) {
+            mLayout_materials.addView(getMaterialsView(item));
+        }
 
         mHandler.sendEmptyMessageDelayed(0, 3000);
     }
@@ -132,6 +156,21 @@ public class MainFragment extends BaseFragment {
         }
     }
 
+    private View getMaterialsView(final MaterialsBo item) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.layout_materials, null);
+        ImageView iv_materials = (ImageView) view.findViewById(R.id.iv_materials);
+        TextView tv_materials = (TextView) view.findViewById(R.id.tv_materials);
+        iv_materials.setImageResource(Integer.valueOf(item.getPicUrl()));
+        tv_materials.setText(item.getName());
+        iv_materials.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(ImageBrowser.getIntent(mContext, item.getPicUrl()));
+            }
+        });
+        return view;
+    }
+
     private class RadioChangedListener implements RadioGroup.OnCheckedChangeListener {
 
         @Override
@@ -139,19 +178,82 @@ public class MainFragment extends BaseFragment {
         }
     }
 
-    private class ProcessAdapter extends CommonAdapter<String> {
+    private class ProcessAdapter extends BaseExpandableListAdapter {
 
-        public ProcessAdapter(Context context, List<String> list, int layoutId) {
-            super(context, list, layoutId);
+        private List<String> mList_group;
+        private List<List<String>> mList_child;
+
+        public ProcessAdapter(List<String> mList_group, List<List<String>> mList_child) {
+            this.mList_group = mList_group;
+            this.mList_child = mList_child;
         }
 
         @Override
-        public void convert(ViewHolder holder, String item, int position) {
-            if (position == 0) {
-                holder.getView(R.id.btn_item_process_finish).setVisibility(View.VISIBLE);
-            }else {
-                holder.getView(R.id.btn_item_process_finish).setVisibility(View.GONE);
+        public int getGroupCount() {
+            return mList_group == null ? 0 : mList_group.size();
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return mList_child == null ? 0 : mList_child.get(groupPosition).size();
+        }
+
+        @Override
+        public Object getGroup(int groupPosition) {
+            return mList_group.get(groupPosition);
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return mList_child.get(groupPosition).get(childPosition);
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.lv_item_process, null);
+            Button btn_finish = (Button) convertView.findViewById(R.id.btn_item_process_finish);
+            if (groupPosition == 0) {
+                btn_finish.setVisibility(View.VISIBLE);
+            } else {
+                btn_finish.setVisibility(View.INVISIBLE);
             }
+            return convertView;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.lv_item_process, null);
+            convertView.setBackgroundResource(R.color.gray);
+            Button btn_finish = (Button) convertView.findViewById(R.id.btn_item_process_finish);
+            if (childPosition == 0) {
+                btn_finish.setText("开始");
+                btn_finish.setVisibility(View.VISIBLE);
+            } else {
+                btn_finish.setVisibility(View.INVISIBLE);
+            }
+            TextView text = (TextView) convertView.findViewById(R.id.tv_item_process_code);
+            text.setText("第" + childPosition + "车床");
+            return convertView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
         }
     }
 
@@ -180,20 +282,18 @@ public class MainFragment extends BaseFragment {
 
         @Override
         public int getCount() {
-            return mList_data == null ? 0 : mList_data.size();
+            return mList_processData == null ? 0 : mList_processData.size();
         }
 
         @Override
-        public Object instantiateItem(ViewGroup container, int position) {
+        public Object instantiateItem(ViewGroup container, final int position) {
             final View view = LayoutInflater.from(mContext).inflate(R.layout.vp_item_main_processbmp, null);
             ImageView imageView = (ImageView) view.findViewById(R.id.iv_item_main_processBmp);
-            imageView.setImageResource(mList_data.get(position));
+            imageView.setImageResource(mList_processData.get(position));
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    mViewPager.setVisibility(View.GONE);
-                    mIv_processBmp.setVisibility(View.VISIBLE);
-                    Glide.with(mContext).load(R.drawable.gif).into(mIv_processBmp);
+                    startActivity(ImageBrowser.getIntent(mContext, mList_processData.get(position) + ""));
                 }
             });
 
@@ -234,7 +334,7 @@ public class MainFragment extends BaseFragment {
             switch (msg.what) {
                 case 0:
                     int currentItem = activity.mViewPager.getCurrentItem();
-                    if (currentItem >= activity.mList_data.size() - 1) {
+                    if (currentItem >= activity.mList_processData.size() - 1) {
                         currentItem = -1;
                     }
                     activity.mViewPager.setCurrentItem(currentItem + 1);
